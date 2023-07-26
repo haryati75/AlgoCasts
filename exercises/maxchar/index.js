80cab5f96eb3707da87220bb4ace22d91db44340
Haryati H <80447592+haryati75@users.noreply.github.com>
2023-07-26 22:38:57 +0800

    solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  // traverse an array and count the number of times a character appears
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  let max = 0;
+  let maxChar = '';
+  // traverse an object and find the character with the highest count
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
