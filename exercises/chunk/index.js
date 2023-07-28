d217dd7d68bbfbc20432e08c59a90b0b48416022
Haryati H <80447592+haryati75@users.noreply.github.com>
2023-07-28 22:40:21 +0800

    solve chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,45 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // Solution 3 - Stephen Grider
+  const chunked = []; // create empty array to hold chunks
+  for (let element of array) {
+    // for each element in the unchunked array ...
+    const last = chunked[chunked.length - 1]; // retrieve the last element in chunked
+
+    if (!last || last.length === size) {
+      // if last element does not exist, or if its length is equal to chunk size
+      chunked.push([element]); // push a new chunk into chunked with the current element
+    } else {
+      // else add the current element into the chunk
+      last.push(element);
+    }
+  }
+  return chunked;
+}
 
 module.exports = chunk;
+
+// Solution 2 - adapted from co-pilot
+// const result = [];
+// // create slices of the array with the given size
+// // push the slices into the result array
+// for (let index = 0; index < array.length; index += size) {
+//   result.push(array.slice(index, index + size));
+// }
+// return result;
+
+// Solution 1 - mine
+// const result = [];
+// let part = [];
+// for (let number of array) { // traverse the array from first to last element
+//   if (part.length < size) {
+//     part.push(number);
+//   } else {
+//     result.push(part);
+//     part = [number];
+//   }
+// }
+// result.push(part); // push remaining part
+// return result;
