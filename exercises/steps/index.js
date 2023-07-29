873c64e4c30015c47b21769a7928eb110323d60b
Haryati H <80447592+haryati75@users.noreply.github.com>
2023-07-29 10:37:49 +0800

    solve steps using repeat and recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -19,20 +19,41 @@
 
 // assumes n is a positive integer > 0
 
-function steps(n) {
-  // Solution #1
-  for (let i = 1; i <= n; i++) {
-    console.log('#'.repeat(i) + ' '.repeat(n - i));
+function steps(n, row = 0, stair = '') {
+  // print n lines
+  // each line has n characters
+
+  // Solution #3 - recursive
+  // base case - row === n (we're done)
+  if (n === row) return;
+
+  // Print completed stair string:
+  // if the stair string has a length === n, print it
+  // increment row with new stair string
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
   }
 
-  // Solution #2
-  // for (let i = 0; i < n; i++) {
-  //   let step = '';
-  //   for (let j = 0; j < n; j++) {
-  //     step += j <= i ? '#' : ' ';
-  //   }
-  //   console.log(step);
-  // }
+  // Assemblies the stair string recursively:
+  // if the length of the stair string is less than or equal to the row number, add a '#'
+  // otherwise add a space
+  stair += stair.length <= row ? '#' : ' ';
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+// Solution #1 - javascript repeat method
+// for (let i = 1; i <= n; i++) {
+//   console.log('#'.repeat(i) + ' '.repeat(n - i));
+// }
+
+// Solution #2 - row, column iteration
+// for (let i = 0; i < n; i++) {
+//   let step = '';
+//   for (let j = 0; j < n; j++) {
+//     step += j <= i ? '#' : ' ';
+//   }
+//   console.log(step);
+// }
