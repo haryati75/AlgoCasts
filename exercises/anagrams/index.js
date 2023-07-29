c870dd0a8a57b300686e4e6c20401c472e91666a
Haryati H <80447592+haryati75@users.noreply.github.com>
2023-07-29 08:07:18 +0800

    solve anagrams solution 2

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,30 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-const buildCharMap = (str) => {
-  const charMap = {};
-  // traverse characters in a string and count the number of times a character appears
-  [...str.replace(/[^\w]/g, '').toLowerCase()].forEach((char) => {
-    charMap[char] = charMap[char] + 1 || 1;
-  });
-  return charMap;
-};
-
 function anagrams(stringA, stringB) {
-  const aCharMap = buildCharMap(stringA);
-  const bCharMap = buildCharMap(stringB);
+  // Solution 2
+  // sort the strings
+  const sortedA = stringA
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+  const sortedB = stringB
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
 
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for (let char in aCharMap) {
-    if (aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  return true;
+  // compare the strings
+  return sortedA === sortedB;
 }
 
 module.exports = anagrams;
+
+// Solution 1
+// const buildCharMap = (str) => {
+//   const charMap = {};
+//   // traverse characters in a string and count the number of times a character appears
+//   // remove spaces and punctuation via regex
+//   [...str.replace(/[^\w]/g, '').toLowerCase()].forEach((char) => {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   });
+//   return charMap;
+// };
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
