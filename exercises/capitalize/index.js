d3ea9193b29a9b2d40d9ed3b36ec98d28a4e6170
Haryati H <80447592+haryati75@users.noreply.github.com>
2023-07-29 09:38:10 +0800

    solve capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,20 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  // Solution 1:
+  // split the string into an array of words
+  // capitalize the first letter of each word
+  // join the words back together
+  return (
+    str &&
+    str
+      .split(' ')
+      .map((word) => {
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(' ')
+  );
+}
 
 module.exports = capitalize;
--- exercises/capitalize/test.js
+++ exercises/capitalize/test.js
@@ -4,6 +4,10 @@ test('Capitalize is a function', () => {
   expect(typeof capitalize).toEqual('function');
 });
 
+test('string is empty', () => {
+  expect(capitalize('')).toEqual('');
+});
+
 test('capitalizes the first letter of every word in a sentence', () => {
   expect(capitalize('hi there, how is it going?')).toEqual(
     'Hi There, How Is It Going?'
