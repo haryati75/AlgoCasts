e16562d848c7c51ca751951af8bd92d5daac850a
Haryati H <80447592+haryati75@users.noreply.github.com>
2023-07-29 10:17:24 +0800

    solve steps

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # AlgoCasts
 
+Forked from Stephen Grider's repo
+
 Companion repo to [The Coding Inteview Bootcamp: Algorithms + Data Structures](https://www.udemy.com/course/coding-interview-bootcamp-algorithms-and-data-structure/)
--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// assumes n is a positive integer > 0
+
+function steps(n) {
+  // Solution #1
+  for (let i = 1; i <= n; i++) {
+    console.log('#'.repeat(i) + ' '.repeat(n - i));
+  }
+
+  // Solution #2
+  // for (let i = 0; i < n; i++) {
+  //   let step = '';
+  //   for (let j = 0; j < n; j++) {
+  //     step += j <= i ? '#' : ' ';
+  //   }
+  //   console.log(step);
+  // }
+}
 
 module.exports = steps;
